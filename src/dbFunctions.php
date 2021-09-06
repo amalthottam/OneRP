a957b57e212fa40eddf8838a2f7975c2b275a063
amal_tmt <amal.mohamed@trimindtech.com>
2021-09-06 15:32:23 +0800

    edited

--- src/dbFunctions.php
+++ src/dbFunctions.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "10.0.2.124";
 $db_username = "normal";
-$db_password = "KaniNa@me$#1";
+$db_password = "1neRp@me$#1";
 $db_name = "onerp";
 $link = mysqli_connect($db_host,$db_username,$db_password,$db_name) or 
         die(mysqli_connect_error());
